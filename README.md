6c17c445d72dcd62bc8128fc8a1811510f385a10
D.Coloma <dnl.coloma@gmail.coma>
2018-01-25 18:29:22 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # tic-et-tac
 Let's play a game...
+
+    Board : c'est le plateau. Il doit y avoir une instance de cette classe lors d'une partie.
+    BoardCase : c'est une case. Il devrait y avoir 9 instances de cette classe dans ta partie
+    Player : ce sont les joueurs. Il doit y avoir 2 instances de cette classe lors d'une partie
+    Game : c'est la partie. Elle initialise tout et lance le jeu
+
+Du coup, je t'invite à dessiner tes classes, que doivent-elles faire, que doivent-elles lancer à leur initialisation ? Chacune des classes doit répondre à un but précis. Nous te conseillons de prendre un peu de temps à dessiner le but de tes classes avant de te lancer à tête baissée dans le code.
+
+
+
+X |   | O
+———
+X |   | O
+———
+X |   | O
+
+
+
+puts "X   X"
+puts " X X "
+puts "  X  "
+puts " X X "
+puts "X   X"
---
+++ app.rb
@@ -0,0 +1,179 @@
+require 'pry'
+class BoardCase
+    attr_accessor :etat
+    attr_reader :number
+    #initialisation : doit être vide
+    def initialize(number)
+        @etat = " "
+        @number=number
+    end
+
+end
+
+class Board
+    #accueille les 9 cases
+    attr_accessor :tab
+    attr_reader :error
+    @@i = true
+
+    def initialize
+
+        @case1 = BoardCase.new(1)
+        @case2 = BoardCase.new(2)
+        @case3 = BoardCase.new(3)
+        @case4 = BoardCase.new(4)
+        @case5 = BoardCase.new(5)
+        @case6 = BoardCase.new(6)
+        @case7 = BoardCase.new(7)
+        @case8 = BoardCase.new(8)
+        @case9 = BoardCase.new(9)
+        @tab = [@case1.etat,@case2.etat,@case3.etat,@case4.etat,@case5.etat,@case6.etat,@case7.etat,@case8.etat,@case9.etat]
+    end
+
+    def state
+        system 'clear'
+        print "\t\t" * 3
+        puts @tab[0..2].join(" | ")#.map{|c| c.etat}.join(" | ")
+        print "\t\t" * 3
+        puts "——+———+——"
+        print "\t\t" * 3
+        puts @tab[3..5].join(" | ")#.map{|c| c.etat}.join(" | ")
+        print "\t\t" * 3
+        puts "——+———+——"
+        print "\t\t" * 3
+        puts @tab[6..8].join(" | ")#.map{|c| c.etat}.join(" | ")
+
+    end
+
+    def plays
+        @error = false
+        @move = gets.chomp.to_i
+        if @tab[@move - 1] == " " #.etat == " "
+            if @@i == true
+                @tab[@move - 1] = 'X'#.etat = 'X'
+                @@i = !@@i
+
+            else
+                @tab[@move - 1] = 'O' #.etat = 'O'
+                @@i = !@@i
+            end
+            @error = false
+
+        else
+            @error = true
+            #binding.pry
+        end
+
+    end
+
+    def victory
+        case @tab[0]
+        when @tab[1]
+            if @tab[0] == @tab[2]
+                puts "WINS !"
+            end
+        when @tab[4]
+            if @tab[0] == @tab[8]
+                puts "WINS !"
+            end
+        when @tab[3]
+            if @tab[0] == @tab[6]
+                puts "WINS !"
+            end
+        end
+
+        case @tab[2]
+        when @tab[4]
+            if @tab[2] == @tab[6]
+                puts "WINS !"
+            end
+        when @tab[5]
+            if @tab[2] == @tab[8]
+                puts "WINS !"
+            end
+        end
+
+        puts "WINS !" if @tab[3] == @tab[4] && @tab[3] == @tab[5]
+
+        puts "WINS !" if @tab[1] == @tab[4] && @tab[1] == @tab[7]
+
+        puts "WINS !" if @tab[6] == @tab[7] && @tab[6] == @tab[8]
+
+    end
+end
+
+
+
+class Player
+    #nom
+    attr_accessor :name
+
+
+    #état victoire ou défaite
+
+
+end
+
+class Game
+    @@turn = false
+    #Crée 2 joueurs
+    def initialize
+        @player1 = Player.new
+        @player2 = Player.new
+        #Initialise de Board
+        @board = Board.new
+        #print "Enter name Player1 : "
+        @player1.name = "J1"#gets.chomp
+        print "Enter name Player2 : "
+        @player2.name = "J2"#gets.chomp
+        system "clear"
+        print "\t" * 3
+        puts "Welcome #{@player1.name} & #{@player2.name} !!! Let's play a game..."
+        print "\t" * 5
+        3.times{
+            print "."
+            #sleep(1)
+        }
+        puts "\n\n\t\t#{@player1.name}, you'll play with the 'X'  &  #{@player2.name}, you'll play with the 'O'"
+        print "\t" * 5
+        3.times{
+            print "."
+            #sleep(1)
+        }
+        puts"\n\n\n\t\t\t  Are you ready ? (press Enter)"
+        print "\t " * 5
+        gets.chomp
+    end
+
+    def play
+
+        @board.state
+        while @board.tab.include? " "
+            @@turn = !@@turn
+            if @@turn == true
+                player = @player1
+            else
+                player = @player2
+            end
+            print "\t\t\t  #{player.name}, your move (1-9): "
+            @board.plays
+            #binding.pry
+            while @board.error
+                @board.state
+                #binding.pry
+                puts "\t\t\t  ERROR, choose an another case"
+                print "\t\t\t  #{player.name}, your move (1-9): "
+                @board.plays
+            end
+            @board.state
+
+        end
+        @board.victory
+        puts "\t\t\t\t\t\t  TIE GAME"
+    end
+
+end
+
+tic_tac = Game.new
+
+tic_tac.play
